d051167285efea49c01a38a9e1eaabd028ecbbbf
prastowoardi <prastowoardi616@gmail.com>
2023-09-02 20:05:37 +0700

    invoke text nama user login dari my account

--- src/test/java/stepDef/login.java
+++ src/test/java/stepDef/login.java
@@ -17,9 +17,30 @@ public class login {
     public void cekLogin() {
         driver = bukaBrowser.getDriver(); // Inisialisasi driver
 
-        WebElement element = driver.findElement(By.xpath("/html[1]/body[1]/div[2]/header[1]/div[1]/div[1]/ul[1]/li[2]/div[1]/ul[1]/li[1]/a[1]")); // Ambil elemen kembali
+        driver.findElement(By.xpath("//div[@class='panel header']//button[@type='button']")).click();
+        driver.findElement(By.xpath("//div[@class='panel wrapper']//li[1]//a[1]")).click();
+        WebElement element = driver.findElement(By.xpath("//div[@class='box-content']//p"));
+        String fullText = element.getText();
 
-        System.out.println("Welcome, Andi Hazz!");
+        // Memisahkan teks menjadi kata-kata
+        String[] words = fullText.split("\\s+"); // Memisahkan berdasarkan spasi
+
+        // Mengambil dua kata pertama untuk nama
+        int numberOfWordsForName = Math.min(2, words.length);
+        StringBuilder nameText = new StringBuilder();
+        for (int i = 0; i < numberOfWordsForName; i++) {
+            nameText.append(words[i]).append(" ");
+        }
+
+        // Mengambil sisa kata-kata sebagai email
+        StringBuilder emailText = new StringBuilder();
+        for (int i = numberOfWordsForName; i < words.length; i++) {
+            emailText.append(words[i]).append(" ");
+        }
+
+        System.out.println("Login sebagai: " + nameText.toString().trim());
+        System.out.println("Email: " + emailText.toString().trim());
+
         driver.quit();
     }
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -6,8 +6,12 @@ Feature: User login ke web Luma
     * Buka halaman "login"
     
   Scenario: Login dengan akun yang sudah dibuat
-    When User memasukkan "emailLogin" dengan "hazz@mail.com"
-    * User memasukkan "passLogin" dengan "a1cab2f5@"
+    When User memasukkan "emailLogin" dengan "<email>"
+    * User memasukkan "passLogin" dengan "<password>"
     * User klik tombol "Sign In"
     * Cek apakah user sudah login
 
+    Examples:
+      | email         | password  |
+#      | raki@mail.con | okP0219!a |
+      | hazz@mail.com | a1cab2f5@ |
